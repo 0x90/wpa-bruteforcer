d1f7a15d317a4290044b0aecba6535fc90dc6190
0x90 <oleg.kupreev@gmail.com>
2016-12-21 18:48:30 +0300

    setup.py, wpa-bruteforcer renamed

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name = "wpa-bruteforce",
+    version = "0.1",
+    author = "SYWorks",
+    description = "WPA connect bruteforce attack tool",
+    keywords = "WiFi 802.11 WPA bruteforcer",
+    url = "https://github.com/SYWorks/wpa-bruteforcer",
+    scripts=['wpa-bruteforcer'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+    ],
+)
